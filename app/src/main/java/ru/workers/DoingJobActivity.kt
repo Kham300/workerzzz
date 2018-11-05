9c8f2223ca7fcfbf1a06ae185daac65f20ea293d
Khamid <boogaga3000@gmail.com>
2018-11-06 01:15:49 +0300

    Initial commit

---
+++ app/src/main/java/ru/workers/DoingJobActivity.kt
@@ -0,0 +1,12 @@
+package ru.workers
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class DoingJobActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activiti_doing_job)
+    }
+}
---
+++ app/src/main/java/ru/workers/ReportsActivity.kt
@@ -0,0 +1,11 @@
+package ru.workers
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class ReportsActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.reports)
+    }
+}
